58c29194665fccff84be1adf75e8adc61914b7f4
Ney Belchior <neybelchior@nomus.com.br>
2021-12-07 11:09:30 -0300

    relação 1 pra 1

---
+++ oo/composicao/Motor.java
@@ -0,0 +1,24 @@
+package oo.composicao;
+
+public class Motor {
+	
+	Carro carro;
+	
+	Motor(Carro carro){
+		this.carro=carro;
+	}
+    
+	
+	boolean ligado =false;
+	
+	double fatorDeInjecao=1;
+
+	int giros() {
+		if(!ligado) {
+			return 0;
+		}else {
+		return (int) Math.round(fatorDeInjecao*3000);
+	}
+		}
+	
+}
