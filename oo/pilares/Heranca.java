af9224a24ee263dfb547441f7d2745b2de827a7f
Ney Belchior <neybelchior@nomus.com.br>
2021-12-08 15:09:00 -0300

    Herança

---
+++ oo/pilares/Heranca.java
@@ -0,0 +1,18 @@
+package oo.pilares;
+
+public class Heranca {
+	
+	/*Reuso de código : Composição    | herança
+	 *                   Tem um(a)       é um (a)
+	 *
+	 *
+	 *
+	 * Como vou saber se eu vou utilizar composição ou herança?
+	 * 1- Sempre devemos priorizar a composição mais do que a herença
+	 * pois a composição é mais flexível do que a herança.
+	 * 
+	 * 
+	 * 
+	 */
+
+}
