4047997cd9e3ecb0d75fe30059b9afe0e2319844
Ney Belchior <neybelchior@nomus.com.br>
2021-12-08 14:05:24 -0300

    Encapsulamento

---
+++ oo/pilares/Encapsulamento.java
@@ -0,0 +1,21 @@
+package oo.pilares;
+
+public class Encapsulamento {
+	//Você colocar algo dentro de uma capsula
+	//A capsula é o próprio objeto
+	
+	/*Utilizando o exemplo do carro o volante está public ,a marcha está public,
+	o motor esta private injeção esta private ou seja ,eu não preciso ter acesso a essa informação
+	muito menos conhecer como é feito o processos internos.
+	
+	Selecionar o que vai ser escondido é muito importante , isso faz parte do processo de encapsular
+	
+	Quanto mais escondido for menor a chance de ter problema entre a relação das coisas
+	
+
+	
+	
+	
+	
+
+}
---
+++ oo/pilares/Teoria.java
@@ -0,0 +1,47 @@
+package oo.pilares;
+
+public class Teoria {
+	
+/* 	Principios : 
+ * 1-Encapsulamento:
+ *   Parte da complexidade do objeto precisa estar escondida para o mundo exterior
+ *   Exemplo:
+ *   Parte do carro você consegue interagir diretamente , por exemplo:
+ *   Volante, direção, marcha
+ *   
+ *   Mas você não precisa saber como fuciona internamente o motor de um carro.
+ *   
+ *   ou seja você não precisa entender a os detalhes internos de um objeto para utilizar um objeto
+ *    
+ *  
+ * 2-Herança
+ * 
+ * Um princípio de reuso de código assim como na composição. A diferença é que
+ * na composição você tem uma relação "tem um"
+ * 
+ * Quando você relaciona, por exemplo :
+ * 
+ * Celta é carro , você tem a relação "é um", civic "é um carro"
+ * via de regra quando você estabelece a relação "É um" você tem uma herença
+ * 
+ * 3-Polimorfismo
+ * 
+ * É a capacidade que você tem de utilizar algum conceito mais genérico  e dentro desse conceito, você colocar coisas mais específicas
+ *Por exemplo :
+ *Eu posso criar no meu sistema um atributo animal e associar esse animal a um cachorro , a um gato, pois animal é mais generico e gato e cachorros são tipos de animais
+ *Ou seja, para HAVE UM POLIMORFISMO PRECISA EXISTIR HERENÇA 
+ * 
+
+ * 
+ * 4-Abstração
+ * Quando você vai desenvolver um sistema você observa o mundo real
+ * observando o mundo real você tem a complexidade do mundo real
+ * e essa complexidade precisa ser simplificada ou abstraida
+ * para você conseguir escrever software que vai corresponder a necessidade do seu negocio
+ * Nem todas as características e comportamentos do seu objeto no mundo real , estarão presentes no seu software
+ * assim como nem todos objtos do mundo real estarão presentes no seu software.
+ * 
+ * 
+ * 
+*/
+}
