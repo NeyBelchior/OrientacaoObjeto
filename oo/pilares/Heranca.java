0d23345d62c67a199189c609c12ee614ff8ea271
Ney Belchior <neybelchior@nomus.com.br>
2021-12-08 15:09:19 -0300

    Herança

--- oo/pilares/Heranca.java
+++ oo/pilares/Heranca.java
@@ -2,14 +2,36 @@ package oo.pilares;
 
 public class Heranca {
 	
-	/*Reuso de código : Composição    | herança
-	 *                   Tem um(a)       é um (a)
-	 *
-	 *
+	/*Reuso de código : Composição        |    herança
+	 *                   Tem um(a)        |      é um (a)
+	 *               -Carro tem um motor  |-civic é um carro
+	 *               -Carro tem portas    |-Gato é um mamífero
+	 *               -Casa tem uma cozinha|-Maça é uma fruta
+                      --é uma referencia
+                      --dentro da classe
+                      --e existe uma colaboracao
+                      --- entre elas
 	 *
 	 * Como vou saber se eu vou utilizar composição ou herança?
 	 * 1- Sempre devemos priorizar a composição mais do que a herença
 	 * pois a composição é mais flexível do que a herança.
+	 * 2- No software é mais preferivel que você tenha mais composição do que herança assim como no mundo real 
+	 * 
+	 * --------------------------------
+	 * 
+	 * Herança(extends): Você vai herdar algumas caracteristicas e comportamentos pré-definidos na classe pai 
+	 * 
+	 * vem sempre do mais genérico para o mais específico
+	 * Super Classe (+Genérica)
+	 * 
+	 * sub classe (+Específica)
+	 * 
+	 * Um classe só pode herdar de uma classe
+	 * 
+	 * É você receber comportamentos e atributos sem necessariamente você precisa reescrever isso dentro da sua classe
+	 * embora exista o conceito de sobrescrever algo @overide
+	 * 
+	 * SE É PRIVATE NÃO É HERDADO POR PADRÃO
 	 * 
 	 * 
 	 * 
