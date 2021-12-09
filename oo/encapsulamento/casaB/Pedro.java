7ffcbbc41a291442c866142a4e1e7f4040de9ae4
Ney Belchior <neybelchior@nomus.com.br>
2021-12-09 18:02:17 -0300

    modificadores de acesso

---
+++ oo/encapsulamento/casaB/Pedro.java
@@ -0,0 +1,20 @@
+package oo.encapsulamento.casaB;
+
+import oo.encapsulamento.casaA.Ana;
+
+public class Pedro extends Ana{
+
+	 void testeAcessos() {
+		
+		 System.out.println(/*mae.*/formaDeFalar);//Estou tentando buscar algo de instancia quando na verdade eu herdo da classe
+		 System.out.println(formaDeFalar);
+		 System.out.println(todosSabem);
+		
+		 A
+		 
+		
+		 
+		 
+		 
+	 }
+}
