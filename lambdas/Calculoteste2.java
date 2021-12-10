17a8c12fb85aa6c6fc3c11c6f75acece8c0fe134
Ney Belchior <neybelchior@nomus.com.br>
2021-12-10 14:20:21 -0300

    Primeira Função Lambda

---
+++ lambdas/Calculoteste2.java
@@ -0,0 +1,15 @@
+package lambdas;
+
+public class Calculoteste2 {
+	public static void main(String[] args) {
+		
+	
+		Calculo calc =(x,y) ->{return x+y;};
+		Calculo calc1 =(x,y) ->{double a = x+y;return a;};
+		
+		System.out.println(calc.executar(2, 3));
+		
+		calc=(x,y) -> x*y;//quando eu não coloco as chaves aquilo que eu declarei depois do -> será automaticamente retornado
+		                  //implicitamente existe um return associado a essa expressão "x*y"
+		System.out.println(calc.executar(2, 3));
+}}
