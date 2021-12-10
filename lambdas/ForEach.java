11b120bfe47c42e2cec30680042c35d0e719272a
Ney Belchior <neybelchior@nomus.com.br>
2021-12-10 17:40:34 -0300

    ForEach Funcional #02

--- lambdas/ForEach.java
+++ lambdas/ForEach.java
@@ -3,8 +3,6 @@ package lambdas;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.naming.Reference;
-
 public class ForEach {
 	public static void main(String[] args) {
 		List <String> aprovados = Arrays
@@ -23,8 +21,17 @@ public class ForEach {
 		//a diferenca desse prode cima é que o processo aqui é mais automatizado,logo, eu não consigo personalizar (colocar !!)
 		aprovados.forEach(System.out::println);
 		
+		System.out.println("\nLambda #02.....");
+		//(nome -> meuImprimir(nome) aqui não precisa de ; pois eu estou passando um parâmetro de uma expressão lambda para o meu foreach
+		aprovados.forEach(nome -> meuImprimir(nome));
 		
-		
+		System.out.println("\nMethod Reference#02...");
+		aprovados.forEach(ForEach::meuImprimir);
+	
+	
 	}
+	static void meuImprimir(String nome) {
+		System.out.println("Oi! Meu nome é "+ nome);}
+	
 
 }
