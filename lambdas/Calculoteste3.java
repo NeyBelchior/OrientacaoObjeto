803e52ac013a16a08d7e92aa9c2b90304d7ed57e
Ney Belchior <neybelchior@nomus.com.br>
2021-12-10 16:39:34 -0300

    Usando interfaces funcionais

--- lambdas/Calculoteste3.java
+++ lambdas/Calculoteste3.java
@@ -1,17 +1,35 @@
 package lambdas;
 
+import java.util.function.BinaryOperator;
+
 public class Calculoteste3 {
 	public static void main(String[] args) {
 		
-	
-		Calculo calc =(x,y) ->{return x+y;};
+	    //Não int>double
+		//double>int
+		//Double a =1.0>um double primitivo para Double (classe warapper)
+		
+		             /*para tipos genricos só é suportado classes*/
+		BinaryOperator<Double> calc =(x,y) ->{return x+y;};
 		Calculo calc1 =(x,y) ->{double a = x+y;return a;};
 		
-		System.out.println(calc.executar(2, 3));
+		System.out.println(calc.apply(2.0, 3.0));
 		
 		calc=(x,y) -> x*y;//quando eu não coloco as chaves aquilo que eu declarei depois do -> será automaticamente retornado
 		                  //implicitamente existe um return associado a essa expressão "x*y"
-		System.out.println(calc.executar(2, 3));
-		System.out.println(calc.legal());
-		System.out.println(Calculo.muitolegal());
+		System.out.println(calc.apply(2.0,3.0));
+		
+		
+		
+		
+		BinaryOperator<Integer> calc2 =(x,y) ->{return x+y;};
+		BinaryOperator<Integer> calc3 =(x,y) ->x+y;
+		Calculo calc4 =(x,y) ->{double a = x+y;return a;};
+		
+		System.out.println(calc2.apply(2, 3));
+		
+		calc=(x,y) -> x*y;
+		                  
+		System.out.println(calc.apply(2.0,3.0));
+		
 }}
