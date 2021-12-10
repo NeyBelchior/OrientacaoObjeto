38d6ffba07cb1361c3664dd17a3394d9b12454af
Ney Belchior <neybelchior@nomus.com.br>
2021-12-10 14:44:25 -0300

    interfaces funcionais

--- lambdas/Calculo.java
+++ lambdas/Calculo.java
@@ -1,7 +1,18 @@
 package lambdas;
-
+@FunctionalInterface
+//eu só posso definir um metodo dentro de uma interface funcional
+//salvo metodo static e o default*****
 public interface Calculo {
-
-	 double executar(double a, double b);
+     
+  /*public abstract ja vem implicito lembrar**/ 
+	double executar(double a, double b);
+	
+	default String legal() {
+		return "legal";
+	}
+	
+	static String muitolegal() {
+		return "muito legal";
+	}
 	
 }
