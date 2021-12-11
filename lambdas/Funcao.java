04dd80a7441d671758509e8f1adcf81e4eb56ed0
Ney Belchior <neybelchior@nomus.com.br>
2021-12-10 21:48:41 -0300

    Function

---
+++ lambdas/Funcao.java
@@ -0,0 +1,49 @@
+package lambdas;
+
+import java.util.function.Function;
+
+public class Funcao {
+	public static void main(String[] args) {
+		
+	
+	
+	Function<Integer, String> parOuImpar =
+			numero -> numero %2 ==0?"Par":"Impar";	
+     
+     
+     Function<String, String> oResultadoE=
+    		 valor -> "O resultado é : " +valor;
+    		 
+    Function<String, String> empolgado =
+    		valor ->valor+"!!!!!!!!!!";
+    		  
+    	Function<String, String> duvida =
+    		    		valor ->valor+"????";
+    		    		 
+    		 
+    	//o aplly passa para o par ou impar e depois o resultado de par o impar passa pra resultadoE e depois oResultadoe passa para empolgado	 
+     String resultadoFinal =parOuImpar
+    		 .andThen(oResultadoE)
+    		 .andThen(empolgado)
+    		 .apply(32);
+     String resultadoFinal2 =parOuImpar
+    		 .andThen(oResultadoE)
+    		 .andThen(duvida)
+    		 .apply(32);
+     
+     System.out.println(resultadoFinal);
+     System.out.println(resultadoFinal2);
+     
+     System.out.println(parOuImpar.apply(32));
+     
+    
+     System.out.println(parOuImpar.apply(32));	
+     	
+     
+     
+     
+			
+	
+
+	}
+	}
