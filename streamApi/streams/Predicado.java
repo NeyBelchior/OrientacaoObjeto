90c6cc3c003486360661ade88fa3a2e534984013
Ney Belchior <neybelchior@nomus.com.br>
2021-12-13 14:06:09 -0300

    Reduce primeiro exercicio

--- streamApi/streams/Predicado.java
+++ streamApi/streams/Predicado.java
@@ -1,6 +1,6 @@
 package streamApi.streams;
 
-@FunctionalInterface
+
 public interface Predicado {
 	
 	 double executa(double a,double b);
---
+++ streamApi/streams/Reduce1.java
@@ -0,0 +1,37 @@
+package streamApi.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class Reduce1 {
+	public static void main(String[] args) {
+		
+		
+		
+		List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9);
+		
+		
+		 BinaryOperator<Integer> soma= (ac,n) -> ac+n;
+		
+		 //sempre que eu tiver um option de inteiro eu tenho que chamaro get()
+		int total1= nums.stream()
+		 .reduce(soma).get();
+		
+		System.out.println(total1);
+		
+		nums.parallelStream().reduce(100,soma);
+		
+		Integer total2 = 	nums.parallelStream().reduce(100,soma);
+		System.out.println(total2);
+		
+		
+		//Resultado foi um optional<Integer>...
+		nums.stream()
+		.filter(n-> n >5)
+		.reduce(soma)
+		.ifPresent(System.out::println);
+		
+	}
+
+}
