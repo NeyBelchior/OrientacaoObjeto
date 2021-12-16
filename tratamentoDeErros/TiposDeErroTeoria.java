b9ec36d9dfe10461559d78990b483070583ad114
Ney Belchior <neybelchior@nomus.com.br>
2021-12-16 07:48:51 -0300

    O básico de erros

--- tratamentoDeErros/TiposDeErroTeoria.java
+++ tratamentoDeErros/TiposDeErroTeoria.java
@@ -2,4 +2,58 @@ package tratamentoDeErros;
 
 public class TiposDeErroTeoria {
 
+	
+	/*Existem basicamente , dois tipos de erros :
+	 * 
+	 * 1-Erros de compilação 
+	 * que são detectados pelo compilador, esse tipo de erro impede que a partir do meu arquivo .java
+	 * eu gere o meu arquivo.class. 
+	 * O próprio complidor detecta o erro.
+	 * O java compila o meu código para um linguagem intermediária que é interpretada pela máquina virtual .java>.class
+	 * 
+	 *
+	  2-Erros de tempo de execução (Runtime)
+	  
+	   Ocorrem quando o meu programa já foi compilado , quando o usuário já está acesando o meu programa .
+	   *Quem chamou o método é o responsável por tataro erro ***<<<<
+	   
+	   
+	   Hierarquia :
+	   
+	   Throwable
+     error         exeception	 (que foge a regra)   
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
 }
